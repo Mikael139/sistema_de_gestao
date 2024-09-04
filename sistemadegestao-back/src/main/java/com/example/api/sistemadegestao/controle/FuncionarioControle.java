058c4fc3b4666f438362a67e24418af2730a0be8
Gustavo Teodoro Gabilan <115747012+GustavoTeodoroGabilan@users.noreply.github.com>
2024-09-04 18:40:28 -0300

    Add files via upload

---
+++ sistemadegestao-back/src/main/java/com/example/api/sistemadegestao/controle/FuncionarioControle.java
@@ -0,0 +1,46 @@
+package com.example.api.sistemadegestao.controle;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.api.sistemadegestao.modelo.FuncionarioModelo;
+import com.example.api.sistemadegestao.modelo.RespostaModelo;
+import com.example.api.sistemadegestao.servico.FuncionarioServico;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class FuncionarioControle {
+    
+    @Autowired
+    private FuncionarioServico fs;
+
+    @DeleteMapping("/removerFuncionario/{codigo}")
+    public ResponseEntity<RespostaModelo> remover(@PathVariable long codigo) {
+        return fs.remover(codigo);
+    }
+
+    @PutMapping("/alterarFuncionario")
+    public ResponseEntity<?> alterar(@RequestBody FuncionarioModelo controleGastos) {
+        return fs.cadastrarAlterar(controleGastos, "alterar");
+    }
+
+    @PostMapping("/cadastrarFuncionario")
+    public ResponseEntity<?> cadastrar(@RequestBody FuncionarioModelo controleGastos) {
+        return fs.cadastrarAlterar(controleGastos, "cadastrar");
+    }
+    //Método para listar todos os produtos
+    @GetMapping("/listarfuncionarios")
+    public Iterable<FuncionarioModelo> listar() {
+        return fs.listar();
+    }
+
+
+}
