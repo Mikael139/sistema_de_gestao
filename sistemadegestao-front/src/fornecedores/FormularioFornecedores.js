dc9b41aabcd8c07678600d9de1d3d3668588b19b
Mikael <mikael.palmeira.111@gmail.com>
2024-09-05 19:28:19 -0300

    reestruturando a classe de fornecedor

--- sistemadegestao-front/src/fornecedores/FormularioFornecedores.js
+++ sistemadegestao-front/src/fornecedores/FormularioFornecedores.js
@@ -3,13 +3,7 @@ function FormularioFornecedores({botao, eventoTeclado, cadastrar, obj, cancelar,
         <form className="menu clientes fornecedores">
             <div className="form-row">
                 <label htmlFor="nomeFornecedor" className="label nome">Fornecedor</label>
-                <input 
-                    type="text" 
-                    value={obj.nomeFornecedor} 
-                    onChange={eventoTeclado} 
-                    name="nomeFornecedor" 
-                    placeholder="Nome do fornecedor" 
-                    className="form-control-first" 
+                <input type="text" value={obj.nomeFornecedor} onChange={eventoTeclado} name="nomeFornecedor" placeholder="Nome do fornecedor" className="form-control-first" 
                 />
             </div>
 
@@ -19,13 +13,7 @@ function FormularioFornecedores({botao, eventoTeclado, cadastrar, obj, cancelar,
                     <label htmlFor="dataCadastro" className="label">Data Cadastro</label>
                 </div>
                 <div className="input-container">
-                    <input 
-                        type="text" 
-                        value={obj.cnpjFornecedor} 
-                        onChange={eventoTeclado} 
-                        name="cnpjFornecedor" 
-                        placeholder="CNPJ do fornecedor" 
-                        className="form-control-sub" 
+                    <input type="text" value={obj.cnpjFornecedor} onChange={eventoTeclado} name="cnpjFornecedor" placeholder="CNPJ do fornecedor" className="form-control-sub" 
                     />
                     <label htmlFor="cnpjFornecedor" className="label">CNPJ</label>
                 </div>
@@ -33,24 +21,12 @@ function FormularioFornecedores({botao, eventoTeclado, cadastrar, obj, cancelar,
 
             <div className="form-row">
                 <div className="input-container">
-                    <input 
-                        type="text" 
-                        value={obj.enderecoFornecedor} 
-                        onChange={eventoTeclado} 
-                        name="enderecoFornecedor" 
-                        placeholder="Endereço do fornecedor" 
-                        className="form-control-sub" 
+                    <input type="text" value={obj.enderecoFornecedor} onChange={eventoTeclado} name="enderecoFornecedor" placeholder="Endereço do fornecedor" className="form-control-sub" 
                     />
                     <label htmlFor="enderecoFornecedor" className="label">Endereço</label>
                 </div>
                 <div className="input-container">
-                    <input 
-                        type="text" 
-                        value={obj.tipoServico} 
-                        onChange={eventoTeclado} 
-                        name="tipoServico" 
-                        placeholder="Tipo de serviço" 
-                        className="form-control-sub" 
+                    <input type="text" value={obj.tipoServico} onChange={eventoTeclado} name="tipoServico" placeholder="Tipo de serviço" className="form-control-sub" 
                     />
                     <label htmlFor="tipoServico" className="label">Tipo de serviço</label>
                 </div>
@@ -58,24 +34,12 @@ function FormularioFornecedores({botao, eventoTeclado, cadastrar, obj, cancelar,
 
             <div className="form-row">
                 <div className="input-container">   
-                    <input 
-                        type="text" 
-                        value={obj.obs} 
-                        onChange={eventoTeclado} 
-                        name="obs" 
-                        placeholder="Observações" 
-                        className="form-control-sub" 
+                    <input type="text" value={obj.obs} onChange={eventoTeclado} name="obs" placeholder="Observações" className="form-control-sub" 
                     />
                 <label htmlFor="obs" className="label">Observações</label>
                 </div>
                 <div className="input-container">
-                    <input 
-                        type="text" 
-                        value={obj.fornecedorContato} 
-                        onChange={eventoTeclado} 
-                        name="fornecedorContato" 
-                        placeholder="Contato do fornecedor" 
-                        className="form-control-sub" 
+                    <input type="text" value={obj.fornecedorContato} onChange={eventoTeclado} name="fornecedorContato" placeholder="Contato do fornecedor" className="form-control-sub" 
                     />
                     <label htmlFor="fornecedorContato" className="label">Contato do fornecedor</label>
                 </div>
@@ -84,32 +48,12 @@ function FormularioFornecedores({botao, eventoTeclado, cadastrar, obj, cancelar,
 
             <div className="button-row">
                 {botao ? (
-                    <input 
-                        type="button" 
-                        value="Cadastrar" 
-                        onClick={cadastrar} 
-                        className="btn btn-primary" 
-                    />
+                    <input type="button" value="Cadastrar" onClick={cadastrar} className="btn btn-primary"/>
                 ) : (
                     <div className="botoes">
-                        <input 
-                            type="button" 
-                            value="Alterar" 
-                            onClick={alterar} 
-                            className="btn btn-warning" 
-                        />
-                        <input 
-                            type="button" 
-                            value="Remover" 
-                            onClick={remover} 
-                            className="btn btn-danger" 
-                        />
-                        <input 
-                            type="button" 
-                            value="Cancelar" 
-                            onClick={cancelar} 
-                            className="btn btn-secondary" 
-                        />
+                        <input type="button" value="Alterar" onClick={alterar} className="btn btn-warning" />
+                        <input type="button" value="Remover" onClick={remover} className="btn btn-danger" />
+                        <input type="button" value="Cancelar" onClick={cancelar} className="btn btn-secondary" />
                     </div>
                 )}
             </div>
